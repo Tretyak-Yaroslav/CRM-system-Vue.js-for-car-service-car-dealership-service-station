d8903cb865814df41566f4f559444dd8e7035895
Третяк Ярослав <y_tretyak@ESO.local>
2019-07-25 16:08:59 +0300

    VueAxios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import { router } from './_helpers'
 import BootstrapVue from 'bootstrap-vue'
 import { TablePlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+    
+Vue.use(VueAxios, axios)
 
 Vue.use(TablePlugin)
 Vue.use(BootstrapVue)
